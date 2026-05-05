01cb9b81bdacb32428df0180429a8a126783ce43
Wilson Mbiuki <wilson.mbiuki@student.moringaschool.com>
2026-05-05 21:33:42 +0300

    Bloglist code

---
+++ src/Bloglist.jsx
@@ -0,0 +1,13 @@
+import BlogPost from "./BlogPost";
+
+function BlogList({ posts }) {
+  return (
+    <div>
+      {posts.map(post => (
+        <BlogPost key={post.id} post={post} />
+      ))}
+    </div>
+  );
+}
+
+export default BlogList;
